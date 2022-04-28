f8f1e1eb8fb67ffb715f983162e9e14696aff5ac
Rick Rieger <rickrieger36251@gmail.com>
2022-04-28 10:40:28 -0400

    Fixed the render of child components with layout

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 const Axios = axios.create({
   baseURL:
-    process.env.REACT_APP_AXIOS === "development"
-      ? "http://localhost:8080/api"
-      : "/api",
+    process.env.REACT_APP_AXIOS === 'development'
+      ? 'http://localhost:8080/api'
+      : '/api',
   timeout: 50000,
 });
 export default Axios;
