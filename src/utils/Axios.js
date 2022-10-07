bfd78a090f536293d01bac587c0d828f6f645790
Rick Rieger <rickrieger36251@gmail.com>
2022-10-07 17:11:57 -0400

    changed address to backend in Axios/Utils

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -3,7 +3,7 @@ const Axios = axios.create({
   baseURL:
     process.env.REACT_APP_AXIOS === 'development'
       ? 'http://localhost:8080/api'
-      : '/api',
+      : 'https://bug-tracker-app-backend.herokuapp.com//api',
   timeout: 50000,
 });
 export default Axios;
